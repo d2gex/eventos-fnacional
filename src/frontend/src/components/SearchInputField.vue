805f978758a0419cb67c3ac3b5cc268feda65745
d2gex <daniel.garcia@d2garcia.com>
2023-04-03 13:49:54 +0200

    Add nested component validation via vee

---
+++ src/frontend/src/components/SearchInputField.vue
@@ -0,0 +1,27 @@
+<template>
+  <Field id="nombreFestejo_id" type="text" :name="name"
+         class="form-control"
+         placeholder="Entra el nombre *"/>
+  <div class="field-error">
+    <ErrorMessage as="div" :name="name"/>
+  </div>
+</template>
+
+<script>
+import {Field, ErrorMessage} from 'vee-validate';
+
+export default {
+  name: 'SearchInput',
+  props:{'name': String},
+  components: {
+    Field,
+    ErrorMessage,
+  },
+  methods:
+      {
+        onSubmit(values) {
+          console.log(JSON.stringify(values, null, 2));
+        }
+      }
+}
+</script>
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,31 +1,40 @@
 <template>
-  <div class="row">
-    <div class="col-md-8">
-      <ToreroForm/>
-    </div>
-    <div class="col-md-4">
-      <GanaderiaForm/>
-    </div>
-  </div>
-  <div class="row">
+<!--  <div class="row">-->
+<!--    <div class="col-md-8">-->
+<!--      <ToreroForm/>-->
+<!--    </div>-->
+<!--    <div class="col-md-4">-->
+<!--      <GanaderiaForm/>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--  <div class="row">-->
+<!--    <div class="col-md-12">-->
+<!--      <FestejosForm/>-->
+<!--    </div>-->
+<!--  </div>-->
+
+    <div class="row">
     <div class="col-md-12">
-      <FestejosForm/>
+      <NestedForm/>
     </div>
   </div>
+
 </template>
 
 <script>
 
-import ToreroForm from '@/components/ToreroForm.vue'
-import GanaderiaForm from '@/components/GanaderiaForm.vue'
-import FestejosForm from "@/components/FestejosForm.vue";
+// import ToreroForm from '@/components/ToreroForm.vue'
+// import GanaderiaForm from '@/components/GanaderiaForm.vue'
+// import FestejosForm from "@/components/FestejosForm.vue";
+import NestedForm from "@/components/NestedForm.vue";
 
 export default {
   name: 'HomeView',
   components: {
-    ToreroForm,
-    GanaderiaForm,
-    FestejosForm
+    NestedForm
+    // ToreroForm,
+    // GanaderiaForm,
+    // FestejosForm
   }
 }
 </script>
