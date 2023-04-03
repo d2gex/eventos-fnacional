be3d58f55d2834120a3c6f12aaa744a4c4f60b4f
d2gex <daniel.garcia@d2garcia.com>
2023-04-03 17:35:40 +0200

    Add Torero, Ganaderia and Festejos components into View

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,42 +1,32 @@
 <template>
-<!--  <div class="row">-->
-<!--    <div class="col-md-8">-->
-<!--      <ToreroForm/>-->
-<!--    </div>-->
-<!--    <div class="col-md-4">-->
-<!--      <GanaderiaForm/>-->
-<!--    </div>-->
-<!--  </div>-->
-<!--  <div class="row">-->
-<!--    <div class="col-md-12">-->
-<!--      <FestejosForm/>-->
-<!--    </div>-->
-<!--  </div>-->
-
-    <div class="row">
+  <div class="row">
+    <div class="col-md-8">
+      <ToreroForm/>
+    </div>
+    <div class="col-md-4">
+      <GanaderiaForm/>
+    </div>
+  </div>
+  <div class="row">
     <div class="col-md-12">
       <FestejosForm/>
     </div>
   </div>
-
 </template>
 
 <script>
 
-// import ToreroForm from '@/components/ToreroForm.vue'
-// import GanaderiaForm from '@/components/GanaderiaForm.vue'
-// import FestejosForm from "@/components/FestejosForm.vue";
-// import NestedForm from "@/components/NestedForm.vue";
+import ToreroForm from '@/components/ToreroForm.vue'
+import GanaderiaForm from '@/components/GanaderiaForm.vue'
 import FestejosForm from "@/components/FestejosForm.vue";
 
 export default {
   name: 'HomeView',
   components: {
-    FestejosForm
-    // NestedForm
-    // ToreroForm,
-    // GanaderiaForm,
-    // FestejosForm
+    FestejosForm,
+    ToreroForm,
+    GanaderiaForm,
+
   }
 }
 </script>
