77e86441aca3765ab2a44bb1d0a291b855bcbfbd
d2gex <daniel.garcia@d2garcia.com>
2023-04-04 16:53:02 +0200

    Add unfoldable list to input

---
+++ src/frontend/src/components/SearchBox.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="input-box">
+    <Field :id="id"
+           :type="type"
+           :name="name"
+           :placeholder="placeholder"
+           v-model="search"
+           @input="onChange"
+           class="form-control"
+    />
+    <ErrorMessage as="div" class="field-error" :name="name"/>
+  </div>
+  <div class="mt-2">
+    <url class="">
+      <li class="d-flex flex-column ml-3 border-bottom" v-for="(item, index) in results" :key="index">
+        <span>{{ item }}</span>
+      </li>
+    </url>
+  </div>
+</template>
+
+<script>
+import {Field, ErrorMessage} from 'vee-validate';
+
+export default {
+  name: 'SearchBox',
+  props: {
+    name: String,
+    id: String,
+    placeholder: String,
+    type: String
+  },
+  components: {
+    Field,
+    ErrorMessage,
+  },
+  methods: {
+    onChange() {
+    }
+  },
+  data() {
+    return {
+      search: '',
+      results: Array(5).fill("Jose Antonio Garcia Gonzalez 'El Litri'").map(v => v.toLowerCase())
+    }
+  }
+}
+</script>
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,32 +1,41 @@
 <template>
-  <div class="row">
-    <div class="col-md-8">
-      <ToreroForm/>
-    </div>
-    <div class="col-md-4">
-      <GanaderiaForm/>
-    </div>
-  </div>
+  <!--  <div class="row">-->
+  <!--    <div class="col-md-8">-->
+  <!--      <ToreroForm/>-->
+  <!--    </div>-->
+  <!--    <div class="col-md-4">-->
+  <!--      <GanaderiaForm/>-->
+  <!--    </div>-->
+  <!--  </div>-->
+  <!--  <div class="row">-->
+  <!--    <div class="col-md-12">-->
+  <!--      <FestejosForm/>-->
+  <!--    </div>-->
+  <!--  </div>-->
   <div class="row">
     <div class="col-md-12">
-      <FestejosForm/>
+      <SearchBox type="text"
+                 id="nombreProfesional_id"
+                 name='nombreProfesional'
+                 placeholder="Entra el festejo *"/>
     </div>
   </div>
 </template>
 
 <script>
 
-import ToreroForm from '@/components/ToreroForm.vue'
-import GanaderiaForm from '@/components/GanaderiaForm.vue'
-import FestejosForm from "@/components/FestejosForm.vue";
+// import ToreroForm from '@/components/ToreroForm.vue'
+// import GanaderiaForm from '@/components/GanaderiaForm.vue'
+// import FestejosForm from "@/components/FestejosForm.vue";
+import SearchBox from "@/components/SearchBox.vue";
 
 export default {
   name: 'HomeView',
   components: {
-    FestejosForm,
-    ToreroForm,
-    GanaderiaForm,
-
+    // FestejosForm,
+    // ToreroForm,
+    // GanaderiaForm,
+    SearchBox
   }
 }
 </script>
